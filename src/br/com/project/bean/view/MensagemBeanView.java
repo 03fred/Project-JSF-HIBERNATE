8286f1752d4912a6c7f78f955329fde146077382
root <root@DESKTOP-6HNAVOF>
2018-07-25 14:09:02 -0300

    Configuração CRUD

--- src/br/com/project/bean/view/MensagemBeanView.java
+++ src/br/com/project/bean/view/MensagemBeanView.java
@@ -23,13 +23,11 @@ public String novo() throws Exception {
 
 @Override
 protected Class<?> getClassImplement() {
-	// TODO Auto-generated method stub
 	return null;
 }
 
 @Override
 protected InterfaceCrud<?> getController() {
-	// TODO Auto-generated method stub
 	return null;
 }
 	
